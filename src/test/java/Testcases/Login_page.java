c5acab5fbd12365a95c45f9c934db91e1337313a
abinayasivasub <abinaya101195@gmail.com>
2024-08-09 17:02:41 +0530

    second commit

--- src/test/java/Testcases/Login_page.java
+++ src/test/java/Testcases/Login_page.java
@@ -1,6 +1,7 @@
 package Testcases;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -38,6 +39,12 @@ public class Login_page
 		Assert.assertEquals(actual_url, "https://www.facebook.com/");
 	}
 	
+	@Test
+	public void validate_forgot_password()
+	{
+		String actual_fp_text=driver.findElement(By.linkText("Forgotten password?")).getText();
+		Assert.assertEquals(actual_fp_text, "Forgotten password?");
+	}
 	@AfterTest
 	public void tearDown()
 	{
